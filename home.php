212557beb9042791133b703654f551fd11e061b5
Deilur <50930623+Deilur@users.noreply.github.com>
2020-05-05 17:28:11 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,7 @@
+echo "# prueba" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Deilur/prueba.git
+git push -u origin master
+                
